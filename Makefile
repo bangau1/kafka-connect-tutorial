f6b195d316702d9c55a7c2efa2de116a4952998c
Agung Pratama <agp.csui08@gmail.com>
2020-11-27 17:46:38 +0700

    WIP

--- Makefile
+++ Makefile
@@ -4,11 +4,14 @@ DB_TARGET_PORT=5433
 bootstrap-source-sql:
 	export PGPASSWORD=postgres; cat sql/source.sql | psql -h localhost -U postgres -d postgres -p $(DB_SOURCE_PORT)
 
-kafka-connect-setup-source-jdbc:
+drop-source-sql:
+	export PGPASSWORD=postgres; echo "DROP table public.users" | psql -h localhost -U postgres -d postgres -p $(DB_SOURCE_PORT)
+
+setup-source-jdbc:
 	curl -d @"kafka-connect/jdbc-source.json" \
 		-H "Content-Type: application/json" \
 		-X POST http://localhost:8083/connectors
 
-kafka-connect-delete-source-jdbc:
+delete-source-jdbc:
 	curl -X DELETE http://localhost:8083/connectors/source_postgresql
 
--- sql/source.sql
+++ sql/source.sql
@@ -2,8 +2,10 @@ create table IF NOT EXISTS public.users(
   id int PRIMARY KEY, 
   name varchar(15), 
   address varchar(15), 
-  timestamp timestamp default current_timestamp
+  timestamp timestamp with time zone default current_timestamp NOT NULL
 );
+
+CREATE INDEX users_timestamp on public.users(timestamp ASC);
 
 INSERT INTO public.users(id, name, address)
 VALUES (1, 'Agung Pratama', 'Cibinong, Bogor')
@@ -11,4 +13,12 @@ ON CONFLICT (id) DO NOTHING;
 
 INSERT INTO public.users(id, name, address)
 VALUES (2, 'Annisa', 'Cibinong, Bogor')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.users(id, name, address)
+VALUES (3, 'Aqilla', 'Cibinong, Bogor')
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO public.users(id, name, address)
+VALUES (4, 'Akbar', 'Serang, Banten')
 ON CONFLICT (id) DO NOTHING;
